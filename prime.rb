5162fe609f3318f74c4c45af45e54404c0f47470
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-01 15:01:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime(integer)
+  PRIME_NUMBERS = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+  results_array = []
+  PRIME_NUMBERS.each do
+    if integer 
+  end
+
+def inefficient_prime(integer)
+  i = 2
+  condition = "neutral"
+  loop.do |integer|
+  until condition == false || i = 2000
+  if integer % i == 0
+    condition = false
+  else
+    i += 1
+  end
+end
+    
